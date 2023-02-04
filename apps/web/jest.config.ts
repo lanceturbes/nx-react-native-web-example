eb14fca38aafe2c7c9a810ef2b0e04c0affb68e0
Lance Turbes <lturbes@mahalobanking.com>
2023-02-04 09:25:27 -0800

    refactor: move and rename web app directory

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'app-web',
+  displayName: 'web',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/app-web',
+  coverageDirectory: '../../coverage/apps/web',
 };
