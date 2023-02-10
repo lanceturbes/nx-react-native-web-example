d84ce8e182b2e668a829fe25fe2484379ffc7f9b
Lance Turbes <lturbes@mahalobanking.com>
2023-02-10 14:40:03 -0800

    feat: create ui-common library

---
+++ libs/ui-common/src/lib/ui-common.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface UiCommonProps {}
+
+export function UiCommon(props: UiCommonProps) {
+    return (
+        <div>
+            <h1>Welcome to UiCommon!</h1>
+        </div>
+    )
+}
+
+export default UiCommon
